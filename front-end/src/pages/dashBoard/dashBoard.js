554d9e5f24d8dcc62c412883302d87e86a6ddccb
H <h@Hs-MacBook-Air.local>
2023-05-01 19:53:59 +0100

    dashboard

--- front-end/src/pages/dashBoard/dashBoard.js
+++ front-end/src/pages/dashBoard/dashBoard.js
@@ -6,6 +6,50 @@ const Dashboard = () => {
     <div className="dash-board">
       <Nav />
       <h2>Dashboard</h2>
+      <>
+        <div className="grid-container">
+          <div className="grid-wrap">
+            <h3>Article 1</h3>
+            <h3>Article 1</h3>
+            <h3>Article 1</h3>
+          </div>
+          <div className="grid-wrap">
+            <h3>Article 2</h3>
+            <h3>Article 2</h3>
+            <h3>Article 2</h3>
+          </div>
+          <div className="grid-wrap">
+            <h3>Article 3</h3>
+            <h3>Article 3</h3>
+            <h3>Article 3</h3>
+          </div>
+          <div className="grid-wrap">
+            <h3>Article 4</h3>
+            <h3>Article 4</h3>
+            <h3>Article 4</h3>
+          </div>
+          <div className="grid-wrap">
+            <h3>Article 5</h3>
+            <h3>Article 5</h3>
+            <h3>Article 5</h3>
+          </div>
+          <div className="grid-wrap">
+            <h3>Article 6</h3>
+            <h3>Article 6</h3>
+            <h3>Article 6</h3>
+          </div>
+          <div className="grid-wrap">
+            <h3>Article 7</h3>
+            <h3>Article 7</h3>
+            <h3>Article 7</h3>
+          </div>
+          <div className="grid-wrap">
+            <h3>Article 8</h3>
+            <h3>Article 8</h3>
+            <h3>Article 8</h3>
+          </div>
+        </div>
+      </>
     </div>
   );
 };
