bc2d03c419677b3625c30e6c6c870ff45d9ac8c9
H <h@Hs-MacBook-Air.local>
2023-05-26 14:21:16 +0100

    protected react routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,33 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./context/hooks/useAuthContext";
+
 import Landing from "./pages/landing/Landing";
 import GetStarted from "./pages/getStarted/GetStarted";
 import WelcomeBack from "./pages/welcomeBack/WelcomeBack";
 import Dashboard from "./pages/dashBoard/dashBoard";
 
 const App = () => {
+  const { user } = useAuthContext();
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/get-started" element={<GetStarted />} />
-        <Route path="/welcome-back" element={<WelcomeBack />} />
-        <Route path="/dash-board" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={!user ? <Landing /> : <Navigate to="/welcome-back" />}
+        />
+        <Route
+          path="/welcome-back"
+          element={!user ? <WelcomeBack /> : <Navigate to="/dash-board" />}
+        />
+        <Route
+          path="/get-started"
+          element={!user ? <GetStarted /> : <Navigate to="/dash-board" />}
+        />
+        <Route
+          path="/dash-board"
+          element={user ? <Dashboard /> : <Navigate to="/welcome-back" />}
+        />
       </Routes>
     </BrowserRouter>
   );
